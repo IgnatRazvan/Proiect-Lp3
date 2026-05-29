1ab292b6dd575a6ac0ef2b633228455562482605
IgnatRazvan <ignatrazvy07@gmail.com>
2026-05-29 12:22:29 +0200

    Add files via upload

--- Proiect.py
+++ Proiect.py
@@ -29,7 +29,35 @@ def adauga_aliment():
         print("Eroare")
 
 
-def afiseaza_raport_si_alerte():
+def afiseaza_lista_alimente():
+    try:
+        f = open(N_FISIER, "r")
+    except Exception:
+        print("Eroare")
+        return
+
+    print("\n--- LISTA TOATE ALIMENTELE ---")
+    linii = f.readlines()
+    f.close()
+
+    if len(linii) == 0:
+        print("Inventarul este gol!")
+        return
+
+    for linie in linii:
+        linie = linie.strip()
+        if not linie:
+            continue
+
+        componente = linie.split(",")
+        nume = componente[0]
+        cantitate = componente[1]
+        data_str = componente[2]
+
+        print("Produs: %s | Cant: %s | Expira: %s" % (nume, cantitate, data_str))
+
+
+def afiseaza_alerte():
     try:
         f = open(N_FISIER, "r")
     except Exception:
@@ -38,8 +66,7 @@ def afiseaza_raport_si_alerte():
 
     data_referinta = datetime(2026, 5, 28)
 
-    print("\n--- STARE INVENTAR (Referinta: 28.05.2026) ---")
-    print("-" * 75)
+    print("\n--- ALERTE SI TIMP RAMAS PANA LA EXPIRARE ---")
 
     linii = f.readlines()
     f.close()
@@ -64,10 +91,10 @@ def afiseaza_raport_si_alerte():
 
             if diferenta < 0:
                 status = "EXPIRAT"
-            elif diferenta <= 3:
-                status = "URGENT (%d zile)" % diferenta
+            elif diferenta == 0:
+                status = "EXPIRA ASTAZI"
             else:
-                status = "In termen"
+                status = "Mai sunt %d zile" % diferenta
 
             print("Produs: %s | Cant: %s | Expira: %s | Status: %s" %
                   (nume, cantitate, data_str, status))
@@ -92,4 +119,48 @@ def sterge_produs_distribuit():
     for linie in linii:
         if linie.strip():
             componente = linie.strip().split(",")
-            if componente[0] == nume_c;
+            if componente[0] == nume_cautat:
+                gasit = True
+            else:
+                linii_ramase.append(linie)
+
+    f = open(N_FISIER, "w")
+    f.writelines(linii_ramase)
+    f.close()
+
+    if gasit:
+        print("Produsul %s a fost eliminat." % nume_cautat)
+    else:
+        print("Eroare")
+
+
+def meniu_principal():
+    initializeaza_stoc()
+
+    while True:
+        print("\n=== SISTEM GESTIUNE RISIPA ALIMENTARA ===")
+        print("1. Adauga produs")
+        print("2. Lista alimente")
+        print("3. Vezi alerte")
+        print("4. Sterge produs")
+        print("5. Iesire")
+
+        optiune = input("Alege optiunea: ")
+
+        if optiune == "1":
+            adauga_aliment()
+        elif optiune == "2":
+            afiseaza_lista_alimente()
+        elif optiune == "3":
+            afiseaza_alerte()
+        elif optiune == "4":
+            sterge_produs_distribuit()
+        elif optiune == "5":
+            print("Aplicatia s-a inchis.")
+            break
+        else:
+            print("Eroare")
+
+
+if __name__ == "__main__":
+    meniu_principal()
